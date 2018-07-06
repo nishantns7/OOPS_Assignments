72008d40f165ceb02e8ebacfae47782d9bc86fef
Nishant <nishant.s@zemosolabs.com>
2018-07-06 12:41:05 +0530

    Assignment

---
+++ mainClass.java
@@ -0,0 +1,75 @@
+import java.io.*;
+class Shape {
+	
+	int width, height;
+	
+	public Shape() {
+		width = 3;
+		height = 4;
+	}
+	
+	public void getPerimeter(double r) {		//Demonstrates method overloading
+		double peri = 2.0 * Math.PI * r;
+		System.out.println("Perimeter of circle: " + peri);
+	}
+
+	public void getPerimeter() {		//Demonstrates method overloading
+		int peri = 4 * width;
+		System.out.println("Perimeter of square: " + peri);
+	}
+	
+	public void getArea(double r) {
+		double area = Math.PI * r * r;
+		System.out.println("Area of circle: " + area);
+	}
+	
+	public void getArea() {
+		int area = width * width ;
+		System.out.println("Area of square: " + area);
+	}
+	
+}
+
+class Square extends Shape {
+
+	public Square() {
+		super();
+	}
+	
+	
+}
+
+class Circle extends Shape {
+	
+	double radius;
+	
+	public Circle(double r) {
+		super();
+		this.radius = r;
+	}
+	
+	public void getPerimeter() {
+		double peri = 2.0 * Math.PI * radius;
+		System.out.println("Output from subclass' getPerimeter: " + peri);
+	}
+	
+	public void getArea() {
+		double area = Math.PI * radius * radius;
+		System.out.println("Output from subclass' getPerimeter: " + area);
+	}
+}
+
+class mainClass {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the radius of the circle as a decimal.");
+		double r = Double.parseDouble(br.readLine());
+		Square square = new Square();
+		Circle circle = new Circle(r);
+		square.getPerimeter();
+		square.getArea();
+		circle.getPerimeter();		//Demonstrates method overriding
+		circle.getArea();		//Demonstrates method overriding
+	}
+}
+
